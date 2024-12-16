2f4f6caa74dce063a0ac82de8bc060f626c830c8
Rafael Santos <rafarag40@gmail.com>
2024-12-16 00:04:45 -0300

    feat: use rocktry for repeat the exe

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from rocketry import Rocketry
+from rocketry.conds import cron
 from utils.get_coins.getDolar import get_dolar
 from utils.get_coins.getEuro import get_euro
 from utils.get_coins.getBitCoin import get_bitcoin
@@ -6,7 +8,15 @@ from utils.get_coins.getEthereum import get_ethereum
 
 print("ola mundo")
 
-get_dolar()
-get_euro()
-get_bitcoin()
-get_ethereum()
+app = Rocketry()
+
+@app.task(cron("*/2 * * * *"))
+def request_coins_values():
+
+    get_dolar()
+    get_euro()
+    get_bitcoin()
+    get_ethereum()
+
+if __name__ == "__main__":
+    app.run()
