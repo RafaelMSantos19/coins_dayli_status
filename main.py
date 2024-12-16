7321e605fe6b1c5ee4a3f6ccbbb8093dc64272b1
Rafael Santos <rafarag40@gmail.com>
2024-12-15 22:44:56 -0300

    feat: import in main file getBitcoin and getEthereum

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from utils.get_coins.getDolar import get_dolar
 from utils.get_coins.getEuro import get_euro
+from utils.get_coins.getBitCoin import get_bitcoin
+from utils.get_coins.getEthereum import get_ethereum
 
 
 print("ola mundo")
 
 get_dolar()
 get_euro()
+get_bitcoin()
+get_ethereum()
